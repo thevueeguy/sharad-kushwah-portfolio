5c6bb36eba5a8b36bfd548280a1f2172fb24a19f
Sharad Kushwah <sharadrkushwah@gmail.com>
2022-11-06 20:03:54 +0530

    responsiveness improved #8

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -17,7 +17,7 @@ export default function BackgroundCircles({}: Props) {
         duration: 3
     }}  
     className="relative flex justify-center items-center"> 
-      <div className="absolute border border-[#896f6f] rounded-full h-[130px] w-[130px] md:h-[200px] md:w-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-[#5f4444] rounded-full h-[130px] w-[130px] md:h-[200px] md:w-[200px] mt-52 animate-ping" />
       <div className="absolute border border-[#333333] opacity-20 rounded-full h-[150px] w-[150px] md:h-[300px] md:w-[300px] mt-52" />
       <div className="absolute border border-[#222121] opacity-25 rounded-full h-[260px] w-[260px] md:h-[520px] md:w-[520px] mt-52" />
       <div className="absolute border border-gray-500 opacity-50 rounded-full h-[350px] w-[350px] md:h-[650px] md:w-[650px] mt-52 animate-pulse" />
