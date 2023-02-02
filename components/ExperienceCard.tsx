12c61ca60b63eef29d046828ed1492be4b53f66a
Sharad Kushwah <sharadrkushwah@gmail.com>
2023-02-02 17:37:57 +0530

    UI fixes

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function ExperienceCard({ experience }: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center p-5 space-y-1 md:space-y-7 md:p-10 flex-shrink-0 w-[325px] md:w-[500px] xl:w-[700px] snap-center bg-[#333] lg:opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center p-5 space-y-1 md:space-y-7 md:p-10 flex-shrink-0 w-[325px] md:w-[500px] xl:w-[700px] snap-center lg:opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
         initial={{
           y: -100,
@@ -23,23 +23,23 @@ export default function ExperienceCard({ experience }: Props) {
           duration: 1,
         }}
         viewport={{ once: true }}
-        className="rounded-full object-cover object-center h-24 w-24 md:h-28 md:w-28 xl:h-36 xl:w-36"
+        className="rounded-full object-cover object-center h-16 w-16 md:h-28 md:w-28 xl:h-36 xl:w-36"
         src={urlFor(experience?.companyImage).url()}
         alt=""
       />
 
-      <div className="px-0 md:px-10">
+      <div className="px-0 pt-2 md:px-10 text-center">
         <h4 className="text-base sm:text-xl md:text-2xl font-light">
           {experience?.jobTitle}
         </h4>
         <p className="text-base sm:text-lg md:text-xl font-bold mt-1">
           {experience?.company}
         </p>
-        <div className="flex space-x-2 my-2">
+        <div className="flex space-x-2 my-2 justify-center">
           {experience?.technologies.map((technology) => (
             <motion.img
               key={technology._id}
-              className="h-10 w-10 rounded-full"
+              className="h-10 w-10 rounded-full bg-white border-b-2 border-t-2"
               src={urlFor(technology.image).url()}
               alt={technology.title}
             />
@@ -52,11 +52,13 @@ export default function ExperienceCard({ experience }: Props) {
             : new Date(experience?.dateEnded).toDateString()}
         </p>
 
-        <ul className="scrollbar-thin pr-2 scrollbar-track-black scrollbar-thumb-red-800">
-          {experience?.points.map((point, index) => (
-            <li key={index}>{point}</li>
-          ))}
-        </ul>
+        <div className="text-left">
+          <ul className="scrollbar-thin pr-2 scrollbar-track-black scrollbar-thumb-red-800 list-disc list-inside ml-5">
+            {experience?.points.map((point, index) => (
+              <li key={index} className="pb-2 text-sm md:text-md lg:text-lg">{point}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </article>
   );
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Skill({ skillSet }: Props) {
   return (
-    <div className="group relative flex cursor-pointer bg-white rounded-full border border-black">
+    <div className="group relative flex cursor-pointer bg-white rounded-full border border-white border-b-2 md:border-b-4 xl:border-b-8 box-border " >
       <motion.img
         initial={{
           opacity: 0,
@@ -18,7 +18,7 @@ export default function Skill({ skillSet }: Props) {
         viewport={{once: true}}
         src={urlFor(skillSet.image).url()}
         alt={skillSet?.title}
-        className="rounded-full border border-gray-500 object-cover h-12 w-12 sm:h-14 sm:w-14 md:h-16 md:w-16 xl:h-20 xl:w-20 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full object-cover h-12 w-12 sm:h-14 sm:w-14 md:h-16 md:w-16 xl:h-20 xl:w-20 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
       <div className="opacity-0 absolute group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-12 w-12 sm:h-14 sm:w-14 md:h-16 md:w-16 xl:h-20 xl:w-20 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
