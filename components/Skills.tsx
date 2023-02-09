125ec9e74c5956e0d99c75d82e6e333112ef8984
Sharad Kushwah <sharadrkushwah@gmail.com>
2023-02-09 20:59:03 +0530

    skill ui fix

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -18,11 +18,11 @@ export default function Skills({ skills }: Props) {
       <h3 className="absolute top-10 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-base sm:text-xl md:text-2xl mx-auto">
         &nbsp;Skills
       </h3>
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-xs sm:text-sm md:text-lg">
+      <h3 className="absolute top-24 uppercase tracking-[3px] text-gray-500 text-xs sm:text-sm md:text-lg">
         Hover over a skill for current proficiency
       </h3>
 
-      <div className="grid grid-cols-4 gap-5 md:gap-7 z-10">
+      <div className="grid grid-cols-4 gap-5 md:gap-7 z-10 mt-10">
         {skills?.map((skill) => (
           <Skill key={skill._id} skillSet={skill} />
         ))}
