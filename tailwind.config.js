b766de55df9732e6f2019d9e6bc795affa16bc9f
Sharad Kushwah <sharadrkushwah@gmail.com>
2023-03-23 22:58:47 +0530

    hero section UI updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "spin-slow": "spin 10s linear infinite",
+        "spin-slow": "spin 15s linear infinite",
+        "spin-slow-reverse": "spin 15s linear infinite reverse",
       },
     },
   },
