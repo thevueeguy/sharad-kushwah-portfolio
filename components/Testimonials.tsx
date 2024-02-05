cbfc519c936c6fd52aaf37510db7a2461cf23773
Sharad Kushwah <sharadrkushwah@gmail.com>
2024-02-05 22:36:45 +0530

    minor change in testimonials order

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -33,7 +33,7 @@ export default function Testimonials({ testimonials }: Props) {
             disableOnInteraction: true,
           }}
         >
-          {testimonials?.sort((first, second) => (first?.name > second?.name ? -1 : 1)).map((testimonial, index) => (
+          {testimonials?.sort((first, second) => (first?.name.split("").reverse().join("") > second?.name.split("").reverse().join("") ? 1 : -1)).map((testimonial, index) => (
             <SwiperSlide key={testimonial._createdAt} virtualIndex={index}>
               <div className="flex justify-center items-center">
                 <div key={testimonial?._id} className="snap-center flex flex-col space-y-5 justify-center items-center px-16 md:p-44 h-screen">
