0720b07fd917d0154fe8db289c56649ed2d0590c
Sharad Kushwah <sharadrkushwah@gmail.com>
2024-04-09 02:32:12 +0530

    minor tweak in testimonials heading

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -17,7 +17,7 @@ type Props = {
 export default function Testimonials({ testimonials }: Props) {
   return (
     <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className="relative h-screen flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly items-center">
-      <h3 className="absolute top-0 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-base sm:text-xl md:text-2xl mx-auto font-medium">&nbsp;Testimonials</h3>
+      <h3 className="absolute top-5 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-base sm:text-xl md:text-2xl mx-auto font-medium">&nbsp;Testimonials</h3>
 
       <motion.div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scrollbar-thin scrollbar-track-red-900/20 scrollbar-thumb-red-900/80 lg:px-16">
         <Swiper
